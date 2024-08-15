5bde0d0a9f4b5e609c7201860897f684be1bf6b5
Abhay <krabhay476@gmail.com>
2024-08-15 14:30:34 +0530

    kk

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -24,6 +25,12 @@ app.use('/api/jobs', require('./routes/jobs'));
 app.use('/api/internships', require('./routes/internships'));
 app.use('/api/courses', require('./routes/courses'));
 app.use('/api/resume', require('./routes/resume'));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+// Serve the React app for any unmatched routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
